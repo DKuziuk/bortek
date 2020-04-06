02d94f243f4f2e1b38d50cdae6859dc2fc97d3b0
DKuziuk <neyden@ukr.net>
2020-04-06 15:35:30 +0300

    release v3.4

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { LanguageProvider } from "./components/langChanger/index.jsx";
+// import Main from './components/main/index';
+import LoginForm from './components/login/index';
+import Order from './components/order/index';
+import "./App.css";
+
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <LanguageProvider className="all">
+                {/* <Main /> */}
+                <LoginForm />
+                <Order />
+            </LanguageProvider>
+        );
+    }
+}
